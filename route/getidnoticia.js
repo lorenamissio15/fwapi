c22caa79ce67e9fddadc83b63c175f45aa2464cc
Marcelo Tomporoski Perez <159451183+Marcelo-IFMS@users.noreply.github.com>
2025-12-09 08:42:24 -0400

    Use ObjectId to parse ID from request parameters

--- route/getidnoticia.js
+++ route/getidnoticia.js
@@ -3,8 +3,8 @@ const { ObjectId } = require("mongodb");
 module.exports = (app) => {
     app.get('/noticias/id/:id', async (req, res) => {
         try {
-            //const id = ObjectId.createFromHexString(req.params.id)
-            const id = req.params.id
+            const id = ObjectId.createFromHexString(req.params.id)
+            
             await app.DBClient.connect(); //realizar a conexão com o banco 
             const noticias = await app.DBClient.db('portalnoticias')
                 .collection('noticias').find({ _id: id }).toArray();
@@ -16,3 +16,4 @@ module.exports = (app) => {
     })
 
 }
+
